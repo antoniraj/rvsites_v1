969d01f2dcc832a85a18da9afc27f1a2cebe3ce6
antonirajsebastion@gmail.com <antonirajsebastion@gmail.com>
2017-03-21 09:34:23 +0530

    web service new

--- application/controllers/Web_service.php
+++ application/controllers/Web_service.php
@@ -35,5 +35,71 @@ class Web_service extends CI_Controller {
         $user = $this->web_service_model->get_user_id($username, $password);
         echo json_encode($user);
     }
+	
+	/**
+     * Retrieves customers details 
+     * @return array
+     * @access public
+     */
+    public function get_customer_details() {
+        // Get post values
+        $customers = $this->web_service_model->get_customers();
+        echo json_encode($customers);
+    }
+	
+	/**
+     * Retrieves attorneys office details 
+     * @return array
+     * @access public
+     */
+    public function get_attorneys_office_details() {
+        // Get post values
+        $attorneys_office = $this->web_service_model->get_attorneys_office();
+        echo json_encode($attorneys_office);
+    }
+	
+	/**
+     * Retrieves location courthouse details 
+     * @return array
+     * @access public
+     */
+    public function get_location_courthouse_details() {
+        // Get post values
+        $location_courthouse = $this->web_service_model->get_location_courthouse();
+        echo json_encode($location_courthouse);
+    }
+	
+	/**
+     * Retrieves location courthouse details 
+     * @return array
+     * @access public
+     */
+    public function get_parking_location_details() {
+        // Get post values
+        $parking_location = $this->web_service_model->get_parking_location();
+        echo json_encode($parking_location);
+    }
+	
+	/**
+     * Retrieves push notification 
+     * @return array
+     * @access public
+     */
+    public function get_push_notification_details() {
+        // Get post values
+        $push_notification = $this->web_service_model->get_push_notification();
+        echo json_encode($push_notification);
+    }
+	
+	/**
+     * Retrieves suggest information
+     * @return array
+     * @access public
+     */
+    public function get_suggest_information_details() {
+        // Get post values
+        $suggest_information = $this->web_service_model->get_suggest_information();
+        echo json_encode($suggest_information);
+    }
 
 }
--- application/models/Web_service_model.php
+++ application/models/Web_service_model.php
@@ -10,6 +10,14 @@ class Web_service_model extends CI_Model {
 
     public $CI;
     public $users = 'users AS US';
+	public $customers = 'customers';
+	public $attorneys_office = 'attorneys_office';
+	public $location_courthouse = 'location_couthouse';
+	public $parking_location = 'parking_location';
+	public $push_notification = 'push_notification';
+	public $suggest_information = 'suggest_information AS SI';
+	
+	
 
     /**
      * Constructor
@@ -60,4 +68,187 @@ class Web_service_model extends CI_Model {
         }
         return $result_array;
     }
+	
+	/*
+     * This method handles getting customer details
+     * @access public
+     * @return type array data
+     * */
+
+    public function get_customers() {
+		$table_name = $this->customers;
+        // get customer details
+        $customer_details_query = $this->db->select('*')
+                ->from($table_name)
+                ->where('status', '1')
+                ->get();
+        $customer_details = $customer_details_query->result();
+		
+		if($customer_details){
+			$customer_data = array(
+						'result'=>true,
+						'msg'=>'',
+						'customer_data'=>$customer_details
+						);
+		} else {
+			$customer_data = array(
+						'result'=>false,
+						'msg'=>'No Records Found',
+						);
+		}
+        return $customer_data;
+    }
+	
+	/*
+     * This method handles getting attorneys office details
+     * @access public
+     * @return type array data
+     * */
+
+    public function get_attorneys_office() {
+		$table_name = $this->attorneys_office;
+        // get customer details
+        $attorneys_office_query = $this->db->select('*')
+                ->from($table_name)
+                ->where('status', '1')
+                ->get();
+        $attorneys_office_details = $attorneys_office_query->result();
+		
+		if($attorneys_office_details){
+			$attorneys_office_data = array(
+						'result'=>true,
+						'msg'=>'',
+						'attorneys_office_data'=>$attorneys_office_details
+						);
+		} else {
+			$attorneys_office_data = array(
+						'result'=>false,
+						'msg'=>'No Records Found',
+						);
+		}
+        return $attorneys_office_data;
+    }
+	
+	/*
+     * This method handles getting location courthouse details
+     * @access public
+     * @return type array data
+     * */
+
+    public function get_location_courthouse() {
+		$table_name = $this->location_courthouse;
+        // get customer details
+        $location_courthouse_query = $this->db->select('*')
+                ->from($table_name)
+                ->where('status', '1')
+                ->get();
+        $location_courthouse_details = $location_courthouse_query->result();
+		
+		if($location_courthouse_details){
+			$location_courthouse_data = array(
+						'result'=>true,
+						'msg'=>'',
+						'location_courthouse_data'=>$location_courthouse_details
+						);
+		} else {
+			$location_courthouse_data = array(
+						'result'=>false,
+						'msg'=>'No Records Found',
+						);
+		}
+        return $location_courthouse_data;
+    }
+	
+	/*
+     * This method handles getting parking location details
+     * @access public
+     * @return type array data
+     * */
+
+    public function get_parking_location() {
+		$table_name = $this->parking_location;
+        // get customer details
+        $parking_location_query = $this->db->select('*')
+                ->from($table_name)
+                ->where('status', '1')
+                ->get();
+        $parking_location_details = $parking_location_query->result();
+		
+		if($parking_location_details){
+			$parking_location_data = array(
+						'result'=>true,
+						'msg'=>'',
+						'parking_location_data'=>$parking_location_details
+						);
+		} else {
+			$parking_location_data = array(
+						'result'=>false,
+						'msg'=>'No Records Found',
+						);
+		}
+        return $parking_location_data;
+    }
+	
+	/*
+     * This method handles getting push notification details
+     * @access public
+     * @return type array data
+     * */
+
+    public function get_push_notification() {
+		$table_name = $this->push_notification;
+        // get customer details
+        $push_notification_query = $this->db->select('*')
+                ->from($table_name)
+                ->where('status', '1')
+                ->get();
+        $push_notification_details = $push_notification_query->result();
+		
+		if($push_notification_details){
+			$push_notification_data = array(
+						'result'=>true,
+						'msg'=>'',
+						'push_notification_data'=>$push_notification_details
+						);
+		} else {
+			$push_notification_data = array(
+						'result'=>false,
+						'msg'=>'No Records Found',
+						);
+		}
+        return $push_notification_data;
+    }
+	
+	/*
+     * This method handles getting suggest information details
+     * @access public
+     * @return type array data
+     * */
+
+    public function get_suggest_information() {
+		$table_name = $this->suggest_information;
+        // get customer details
+        $suggest_information_query = $this->db->select('SI.*,US.email')
+                ->from($table_name)
+				->join($this->users, 'SI.user_id = US.id', 'inner')
+                //->where('status', '1')
+                ->get();
+        $suggest_information_details = $suggest_information_query->result();
+		
+		if($suggest_information_details){
+			$suggest_information_data = array(
+						'result'=>true,
+						'msg'=>'',
+						'suggest_information_data'=>$suggest_information_details
+						);
+		} else {
+			$suggest_information_data = array(
+						'result'=>false,
+						'msg'=>'No Records Found',
+						);
+		}
+        return $suggest_information_data;
+    }
+	
+	
 }
